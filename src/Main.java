0114b76a9b5c1a796dfd7e2e41ab2aa519f95a04
Orlov-D <82273356+Orlov-D@users.noreply.github.com>
2021-05-18 15:41:37 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ public class Main {
         double weight = 66.20; //в киллограммах
         double height = 180.50; //в сантиметрах
 
-        double BMI = service.calculate(weight, height);
-        System.out.println("Индекс Массы Тела = " + BMI);
+        double bmi = service.calculate(weight, height);
+        System.out.println("Индекс Массы Тела = " + bmi);
 
         weight = 70.00;
         height = 160.50;
-        BMI = service.calculate(weight, height);
-        System.out.println("Индекс Массы Тела = " + BMI);
+        bmi = service.calculate(weight, height);
+        System.out.println("Индекс Массы Тела = " + bmi);
 
-        BMI = service.calculate(60.10, 172.30);
-        System.out.println("Индекс Массы Тела = " + BMI);
+        bmi = service.calculate(60.10, 172.30);
+        System.out.println("Индекс Массы Тела = " + bmi);
     }
 }
